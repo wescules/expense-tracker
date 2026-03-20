ce39dee4e8bffb9136f76c95c07b858aea91862d
wescules <wafermonster@yahoo.com>
2026-03-20 18:38:00 +0800

    service worker is so fast. very good for offline use. order matters and things were breaking so i had to reorder the assets because JS is a dumb interpreted language with trash DLL

--- sw.js
+++ sw.js
@@ -1,14 +1,18 @@
-const CACHE_NAME = 'v4-latest';
+const CACHE_NAME = 'v5-order-matters';
+// Order matters here - critical assets first, then non-critical ones. 
+// Found out the hard way that if the JS files are cached before the HTML/CSS, the app can break on first load since it tries to fetch uncached HTML/CSS.
 const ASSETS = [
   '/expense-tracker/',
+  // html first for faster load
   '/expense-tracker/index.html',
-  '/expense-tracker/js/index.js',
-  '/expense-tracker/js/helper/functions.js',
-  '/expense-tracker/js/helper/database.js',
-  '/expense-tracker/js/helper/renderCharts.js',
-  '/expense-tracker/js/helper/renderTables.js',
-  '/expense-tracker/js/helper/categoryPicker.js',
-  '/expense-tracker/js/helper/settings.js',
+  // css and fonts so it renders styled immediately
+  '/expense-tracker/css/style.css',
+  '/expense-tracker/css/fa.min.css',
+  '/expense-tracker/webfonts/fa-brands-400.woff2',
+  '/expense-tracker/webfonts/fa-regular-400.woff2',
+  '/expense-tracker/webfonts/fa-solid-900.woff2',
+  '/expense-tracker/webfonts/fa-v4compatibility.woff2',
+  // JS files last since they are not critical for initial render
   '/expense-tracker/js/externalLibraries/cal-heatmap.min.js',
   '/expense-tracker/js/externalLibraries/chart.min.js',
   '/expense-tracker/js/externalLibraries/chartjs-adapter-luxon.js',
@@ -16,17 +20,20 @@ const ASSETS = [
   '/expense-tracker/js/externalLibraries/luxon.min.js',
   '/expense-tracker/js/externalLibraries/popper.js',
   '/expense-tracker/js/externalLibraries/tooltip.js',
-  '/expense-tracker/css/style.css',
-  '/expense-tracker/css/fa.min.css',
+  // App code
+  '/expense-tracker/js/helper/database.js',
+  '/expense-tracker/js/helper/functions.js',
+  '/expense-tracker/js/helper/categoryPicker.js',
+  '/expense-tracker/js/helper/settings.js',
+  '/expense-tracker/js/helper/renderCharts.js',
+  '/expense-tracker/js/helper/renderTables.js',
+  '/expense-tracker/js/index.js',
+  // images and non-critical assets
   '/expense-tracker/manifest.json',
   '/expense-tracker/pwa/icon-192.png',
   '/expense-tracker/pwa/icon-512.png',
   '/expense-tracker/assets/wes.webp',
   '/expense-tracker/assets/abbie.webp',
-  '/expense-tracker/webfonts/fa-brands-400.woff2',
-  '/expense-tracker/webfonts/fa-regular-400.woff2',
-  '/expense-tracker/webfonts/fa-solid-900.woff2',
-  '/expense-tracker/webfonts/fa-v4compatibility.woff2',
 ];
 
 // Install → cache critical assets
